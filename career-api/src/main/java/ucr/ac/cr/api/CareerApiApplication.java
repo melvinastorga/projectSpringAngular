7341e3b111baf32ea65551bdbb7e6b4bf94dbcc3
melvinastorga <31803448+melvinastorga@users.noreply.github.com>
2020-06-20 10:03:44 -0600

    create api spring

---
+++ career-api/src/main/java/ucr/ac/cr/api/CareerApiApplication.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerApiApplication.class, args);
+	}
+
+}
---
+++ career-api/src/test/java/ucr/ac/cr/api/CareerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
