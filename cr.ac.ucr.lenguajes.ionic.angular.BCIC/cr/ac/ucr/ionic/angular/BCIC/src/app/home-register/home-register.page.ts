2ce712932ee75633bda5b70a16664390caabdff8
Melissa <meli8538@gmail.com>
2020-07-07 18:50:08 -0600

    Ready locations dropdowns

--- cr.ac.ucr.lenguajes.ionic.angular.BCIC/cr/ac/ucr/ionic/angular/BCIC/src/app/home-register/home-register.page.ts
+++ cr.ac.ucr.lenguajes.ionic.angular.BCIC/cr/ac/ucr/ionic/angular/BCIC/src/app/home-register/home-register.page.ts
@@ -1,24 +1,64 @@
-import { Component, OnInit, Inject } from "@angular/core";
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { Component, OnInit, Inject, Input } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from "@angular/material/dialog";
+import { RestService } from '../rest.service';
+import { AlertController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
 
-export interface DialogData {
-  animal: string;
-  name: string;
-}
+
+
 @Component({
   selector: "app-home-register",
   templateUrl: "./home-register.page.html",
   styleUrls: ["./home-register.page.scss"],
 })
 export class HomeRegisterPage implements OnInit {
-  constructor(
+  @Input() studentData = { provinceId :0, cantonId:0 };
+
+
+  constructor( public rest:RestService, public dialog: MatDialog, public alertController: AlertController, private route:ActivatedRoute, private router:Router,
     public dialogRef: MatDialogRef<HomeRegisterPage>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData
+    
   ) {}
+  provinces: any=[]
+  cantons: any=[]
+  districts: any=[]
+  provinceId:0
+  cantonId:0
 
-  ngOnInit() {}
-
-  onNoClick(): void {
-    this.dialogRef.close();
+  ngOnInit() {
+    this.getProvince();
   }
+
+
+  getProvince(){
+    this.rest.getProvince().subscribe((data)=>{
+      this.provinces = data
+    });
+  }
+
+getCanton(provinceId){
+    this.rest.getCanton(provinceId).subscribe((data)=>{
+      this.cantons = data
+      this.ngOnInit();
+  });
+  }
+
+  getDistrict(){
+    console.log(this.provinceId, this.cantonId)
+  this.rest.getDistric(this.provinceId, this.cantonId).subscribe((data)=>{
+    this.districts = data
+    this.ngOnInit();
+});
+}
+
+  /*
+  $scope.getCanton = function() {
+    this.rest.getCanton(provinceId).subscribe((data)=>{
+      this.canton = data
+      this.ngOnInit();
+      console.log(data)
+    });
+  }
+  */
+
 }
